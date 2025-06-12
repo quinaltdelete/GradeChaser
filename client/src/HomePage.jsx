1c4ded2da008145686497ded0966be972228d3d8
Quinault Childs <quinaltdelete@gmail.com>
2025-06-12 11:35:29 -0700

    more

--- client/src/HomePage.jsx
+++ client/src/HomePage.jsx
@@ -5,7 +5,7 @@ const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5001";
 function HomePage({ routes }) {
   return (
     <div>
-      <RankingDisplay routes={routes} />
+      <RankingDisplay routes={routes} user={user} />
     </div>
   );
 }
